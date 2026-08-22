ea25b40f747fcd8843b29ec4173789bfa9f6624d
Marcelo <marcelopetitotr@gmail.com>
2026-08-22 17:50:08 -0300

    Estado funcional antes da containerização
    Algumas alterações em arquivos foram feitas pra preparar o projeto para containerização

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import requests
+import os
 
 # URL da API FastAPI (rodando localmente)
-API_URL = "http://localhost:8000"
+# API_URL = "http://localhost:8000"
+API_URL = os.getenv("API_URL", "http://localhost:8000")
 
 st.set_page_config(
     page_title="Agente Corporativo IA", page_icon="🧠", layout="centered"
